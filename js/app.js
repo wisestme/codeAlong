ce71867b995acf03820c162945ab1750c4263c19
wisestMe <wyhzest@gmail.com>
2019-02-19 18:51:46 +0100

    adds a simple fizzBuzz

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-alert('King Jesus');
-confirm('I love Jesus');
-prompt("Do you love Jesus?");
+/*
+let answer = prompt('who is the wisest man?');
+let ans = answer.toUpperCase();
+if(ans === "SOLOMON") {
+	alert('you are very sharp');
+} else if(ans === 'CHIJIOKE') {
+	alert('you are very sharp');
+} else {
+	alert('are you a murunkus ?' + answer + ' is not correct');
+}
+*/
+
+let randomNumber = Math.round(Math.random() * 5);
+
+if(randomNumber % 3 === 0){
+	alert('fizz');
+}else if(randomNumber % 5 === 0) {
+	alert('buzz');
+} else {
+	alert(randomNumber);
+}
