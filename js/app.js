ed98439914142fb90ee372b2824f6d3448c48674
Chijioke Nwagwu <wyhzest@gmail.com>
2019-03-16 00:15:37 +0000

    adds function to get score

--- js/app.js
+++ js/app.js
@@ -537,6 +537,23 @@ function getCardNumericValue(card) {
 		break;
 	}
 }
+
+// function to get score
+function getScore(cardArray) {
+	let score = 0;
+	let hasAce = false;
+	for (let i = 0; i < cardArray.length; i++) {
+		let card = cardArray[i];
+		score += getCardNumericValue(card);
+		if (card.values === 'Ace') {
+			hasAce = true;
+		}
+	}
+	if (hasAce && score + 10 <= 21) {
+		return score + 10;
+	}
+	return score;
+}
 //shuffleDeck();
 
 function showStatus(){
